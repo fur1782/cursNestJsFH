08681e040a0d01850f97ba8c4a3f93aa3b2028ba
Fur1782 <fur1782@gmail.com>
2026-01-28 11:01:10 +0100

    s7. 75 - 79

--- 03-pokedex/src/pokemon/dto/create-pokemon.dto.ts
+++ 03-pokedex/src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,14 @@
-export class CreatePokemonDto {}
+import { IsInt, IsPositive, IsString, Min } from "class-validator";
+
+export class CreatePokemonDto {
+
+    // isInt, isPositive, min 1
+    @IsInt()
+    @IsPositive()
+    @Min(1)
+    no: number;
+
+    // isString 
+    @IsString()
+    name: string;
+}
