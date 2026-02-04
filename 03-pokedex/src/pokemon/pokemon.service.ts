8b766d6583869d2d0d8c247fc245aead6eb07a62
Agusti <agusti.rodelas@ctfc.cat>
2026-02-04 17:17:42 +0100

    Fi de la secció 7

--- 03-pokedex/src/pokemon/pokemon.service.ts
+++ 03-pokedex/src/pokemon/pokemon.service.ts
@@ -20,11 +20,7 @@ export class PokemonService {
         const pokemon = await this.pokemonModel.create(createPokemonDto);
         return pokemon;
     } catch (error) {
-      if(error.code === 11000) {
-        throw new BadRequestException(`Pokemon exists in db ${JSON.stringify(error.keyValue)}`);
-      }
-      console.log(error);
-      throw new InternalServerErrorException(`Can't create Pokemon - Check server logs`);
+      this.handleExceptions(error);
     }
   }
 
@@ -57,11 +53,42 @@ export class PokemonService {
     return pokemon;
   }
 
-  update(id: number, updatePokemonDto: UpdatePokemonDto) {
-    return `This action updates a #${id} pokemon`;
+  async update(term: string, updatePokemonDto: UpdatePokemonDto) {
+
+
+    const pokemon = await this.findOne(term);
+    
+    if(updatePokemonDto.name) {
+      updatePokemonDto.name = updatePokemonDto.name.toLowerCase();
+    }
+
+    try {
+
+      await pokemon.updateOne(updatePokemonDto);
+      return {...pokemon.toJSON(), ...updatePokemonDto};
+    } catch (error) {
+      this.handleExceptions(error);
+    }
+
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} pokemon`;
+  async remove(id: string) {
+    // const pokemon = await this.findOne(id);
+    // await pokemon.deleteOne();
+    // const result = await this.pokemonModel.findByIdAndDelete(id);
+    const {deletedCount} = await this.pokemonModel.deleteOne({ _id: id });
+    if (deletedCount === 0) {
+      throw new BadRequestException(`Pokemon with id "${id}" not found`);
+    }
+    return; 
+  }
+
+
+  private handleExceptions( error: any ) {
+    if (error.code === 11000) {
+        throw new BadRequestException(`Pokemon exists in db ${JSON.stringify(error.keyValue)}`);
+      }
+      console.log(error);
+      throw new InternalServerErrorException(`Can't update Pokemon - Check server logs`);
   }
 }
