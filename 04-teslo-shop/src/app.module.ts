fef4c5964b56344b0cea446f3efb793ec047116c
Fur1782 <fur1782@gmail.com>
2026-02-24 20:15:20 +0100

    secció 10 finalitzada

--- 04-teslo-shop/src/app.module.ts
+++ 04-teslo-shop/src/app.module.ts
@@ -1,6 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './products/products.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: +(process.env.DB_PORT || 5432),
+      database: process.env.POSTGRES_DB,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    ProductsModule,
+    CommonModule
+  ],
 })
 export class AppModule {}
